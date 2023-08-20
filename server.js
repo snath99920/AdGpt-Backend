d62c6fe8539c0f43aec8c87264574fdce050b1dd
sonthenap2e <veerabhadra.sonthena@p2epl.com>
2023-08-20 13:44:08 +0530

    added backend

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 // This is your test secret API key.
-const express=require("express");
-const dotenv= require("dotenv");
-var cors = require('cors')
-const port =4242;
+const express = require("express");
+const dotenv = require("dotenv");
+var cors = require("cors");
+const port = 4243;
 const app = express();
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
+app.use("/", require("./paymentRoute.js"));
 
-app.use("/",require("./paymentRoute.js"));
-
-
-app.listen(port, () => console.log('Running on port', port));
+app.listen(port, () => console.log("Running on port", port));
